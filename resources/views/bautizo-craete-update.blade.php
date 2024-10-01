2f1106560e08aeb5876a273ed48c7ac412a0d50b
axelherreram <aherreram22@miumg.edu.gt>
2024-09-30 18:09:24 -0600

    plantilla de crear bautizo y modificacion a list

---
+++ resources/views/bautizo-craete-update.blade.php
@@ -0,0 +1,96 @@
+@extends('layouts.app')
+
+@section('style')
+    <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
+    <style>
+        .card{
+            max-width: 800px;
+            margin: 0 auto;
+        }
+    </style>
+@endsection
+
+@section('wrapper')
+    <div class="page-wrapper">
+        <div class="page-content">
+            <div class="card radius-10 ">
+                <div class="card-header bg-transparent">
+                    <a href="dashboard" class="text-primary">
+                        <i class="font-22 lni lni-arrow-left"></i>
+                        Regresar
+                    </a>
+                    <h3 class="mt-3">Crear nuevo bautizo</h3>
+                </div>
+
+                <form class="p-4">
+                    <div class="row mb-3">
+                        <label for="fecha" class="col-sm-3 col-form-label">Fecha del Bautizo:</label>
+                        <div class="col-sm-9">
+                            <input type="date" class="form-control" id="fecha" name="fecha">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <label for="nombre_bautizado" class="form-label">Nombres de la persona bautizada:</label>
+                            <input type="text" class="form-control" id="nombre_bautizado" name="nombre_bautizado">
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="apellido_bautizado" class="form-label">Apellidos de la persona bautizada:</label>
+                            <input type="text" class="form-control" id="apellido_bautizado" name="apellido_bautizado">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="fecha_nacimiento" class="col-sm-3 col-form-label">Fecha de nacimiento:</label>
+                        <div class="col-sm-9">
+                            <input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <label for="nombre_padre" class="form-label">Nombre del padre:</label>
+                            <input type="text" class="form-control" id="nombre_padre" name="nombre_padre">
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="nombre_madre" class="form-label">Nombre de la madre:</label>
+                            <input type="text" class="form-control" id="nombre_madre" name="nombre_madre">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <label for="municipio" class="form-label">Municipio:</label>
+                            <input type="text" class="form-control" id="municipio" name="municipio">
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="departamento" class="form-label">Departamento:</label>
+                            <input type="text" class="form-control" id="departamento" name="departamento">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label for="padrinos" class="col-sm-3 col-form-label">Padrinos:</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="padrinos" name="padrinos">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="d-flex justify-content-end">
+                            <button type="submit" class="btn btn-primary w-25">Guardar</button>
+                        </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('script')
+    <!--plugins-->
+    <script src="assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
+    <script src="assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <script src="assets/plugins/chartjs/js/chart.js"></script>
+    <script src="assets/plugins/sparkline-charts/jquery.sparkline.min.js"></script>
+    <!--Morris JavaScript -->
+    <script src="assets/plugins/raphael/raphael-min.js"></script>
+    <script src="assets/plugins/morris/js/morris.js"></script>
+    <script src="assets/js/index2.js"></script>
+@endsection
