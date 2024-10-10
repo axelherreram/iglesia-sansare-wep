dba9c864ac9876130c298d325db9ca9903ef1df1
axelherreram <aherreram22@miumg.edu.gt>
2024-10-10 16:00:02 -0600

    Ajuste del formulario a el diseño

--- resources/views/comunion-craete-update.blade.php
+++ resources/views/comunion-craete-update.blade.php
@@ -3,7 +3,7 @@
 @section('style')
     <link href="assets/plugins/vectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet" />
     <style>
-        .card{
+        .card {
             max-width: 800px;
             margin: 0 auto;
         }
@@ -22,31 +22,57 @@
                 </div>
 
                 <form class="p-4">
-                    <!-- Fecha de la comunión -->
+                    <!-- Correlativo y Fecha del Comunión -->
                     <div class="row mb-3">
-                        <label for="fecha" class="col-sm-3 col-form-label">Fecha de la comunión:</label>
-                        <div class="col-sm-9">
-                            <input type="date" class="form-control" id="fecha" name="fecha" aria-label="Fecha de la comunión">
+                        <div class="col-sm-6">
+                            <label for="correlativo" class="form-label">Partida No:</label>
+                            <input type="text" class="form-control" id="correlativo" name="correlativo">
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="fecha_bautizo" class="form-label">Fecha de Comunión:</label>
+                            <input type="date" class="form-control" id="fecha_bautizo" name="fecha_bautizo">
                         </div>
                     </div>
-                    
+
                     <!-- Datos Persona de la primera comunión -->
                     <div class="row mb-3">
-                        <div class="col-sm-6">
-                            <label for="nombre_comunion" class="form-label">Nombre de la persona de la primera comunión:</label>
-                            <input type="text" class="form-control" id="nombre_comunion" name="nombre_comunion" aria-label="Nombre de la persona de la primera comunión">
-                        </div>
-                        <div class="col-sm-6">
-                            <label for="apellido_comunion" class="form-label">Apellidos de la persona de la primera comunión:</label>
-                            <input type="text" class="form-control" id="apellido_comunion" name="apellido_comunion" aria-label="Apellidos de la persona de la primera comunión">
+                        <div class="col-sm-12">
+                            <label for="nombre_comunion" class="form-label">Nombre de la persona de la primera
+                                comunión:</label>
+                            <input type="text" class="form-control" id="nombre_comunion" name="nombre_comunion"
+                                aria-label="Nombre de la persona de la primera comunión">
                         </div>
                     </div>
-                    
+                    <span> <strong>Datos Sacerdote</strong></span>
+                    <hr>
+                    <div class="row mb-3">
+                        <div class="col-sm-12">
+                            <label for="nombre_comunion" class="form-label">Nombre de la persona de la primera
+                                comunión:</label>
+                            <input type="text" class="form-control" id="nombre_comunion" name="nombre_comunion"
+                                aria-label="Nombre de la persona de la primera comunión">
+                        </div>
+                    </div>
+                    <span> <strong>Datos padres</strong></span>
+                    <hr>
+                    <!-- Datos de los Padres -->
+                    <div class="row mb-3">
+                        <div class="col-sm-6">
+                            <label for="nombre_padre" class="form-label">Nombre del padre:</label>
+                            <input type="text" class="form-control" id="nombre_padre" name="nombre_padre">
+                        </div>
+                        <div class="col-sm-6">
+                            <label for="nombre_madre" class="form-label">Nombre de la madre:</label>
+                            <input type="text" class="form-control" id="nombre_madre" name="nombre_madre">
+                        </div>
+                    </div>
+
                     <!-- Ubicación -->
                     <div class="row mb-3">
                         <div class="col-sm-6">
                             <label for="departamento" class="form-label">Departamento:</label>
-                            <select class="form-control" id="departamento" name="departamento" aria-label="Seleccione el departamento">
+                            <select class="form-control" id="departamento" name="departamento"
+                                aria-label="Seleccione el departamento">
                                 <option value="">Seleccione el departamento</option>
                                 <option value="departamento1">Departamento 1</option>
                                 <option value="departamento2">Departamento 2</option>
@@ -56,7 +82,8 @@
                         </div>
                         <div class="col-sm-6">
                             <label for="municipio" class="form-label">Municipio:</label>
-                            <select class="form-control" id="municipio" name="municipio" aria-label="Seleccione el municipio">
+                            <select class="form-control" id="municipio" name="municipio"
+                                aria-label="Seleccione el municipio">
                                 <option value="">Seleccione el municipio</option>
                                 <option value="municipio1">Municipio 1</option>
                                 <option value="municipio2">Municipio 2</option>
@@ -66,21 +93,7 @@
                         </div>
                     </div>
 
-                    <span><strong>Persona participante</strong></span>
-                    <hr>
-                    
-                    <!-- Datos Persona de la primera comunión -->
-                    <div class="row mb-3">
-                        <div class="col-sm-6">
-                            <label for="nombre_participante" class="form-label">Nombre de la persona participante:</label>
-                            <input type="text" class="form-control" id="nombre_participante" name="nombre_participante" aria-label="Nombre de la persona participante">
-                        </div>
-                        <div class="col-sm-6">
-                            <label for="apellido_participante" class="form-label">Apellidos de la persona participante:</label>
-                            <input type="text" class="form-control" id="apellido_participante" name="apellido_participante" aria-label="Apellidos de la persona participante">
-                        </div>
-                    </div>
-                    
+
                     <div class="row">
                         <div class="d-flex justify-content-end">
                             <button type="submit" class="btn btn-primary w-25 w-sm-100">Guardar</button>
