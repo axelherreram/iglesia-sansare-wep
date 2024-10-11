1b41084a4811ade0170ddb911caea13d96e7f9be
axelherreram <aherreram22@miumg.edu.gt>
2024-10-10 21:09:45 -0600

    Boton de regresar en todas las vistas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,65 +2,80 @@
 <html lang="en">
 
 <head>
-	<!-- Required meta tags -->
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!--favicon-->
-	<link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
-	<!--plugins-->
-	@yield("style")
-	<link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
-	<link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
-	<link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
-	<!-- loader-->
-	<link href="assets/css/pace.min.css" rel="stylesheet" />
-	<script src="assets/js/pace.min.js"></script>
-	<!-- Bootstrap CSS -->
-	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--favicon-->
+    <link rel="icon" href="assets/images/favicon-32x32.png" type="image/png" />
+    <!--plugins-->
+    @yield('style')
+    <link href="assets/plugins/simplebar/css/simplebar.css" rel="stylesheet" />
+    <link href="assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css" rel="stylesheet" />
+    <link href="assets/plugins/metismenu/css/metisMenu.min.css" rel="stylesheet" />
+    <!-- loader-->
+    <link href="assets/css/pace.min.css" rel="stylesheet" />
+    <script src="assets/js/pace.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/bootstrap-extended.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-	<link href="assets/css/app.css" rel="stylesheet">
-	<link href="assets/css/icons.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link href="assets/css/app.css" rel="stylesheet">
+    <link href="assets/css/icons.css" rel="stylesheet">
 
     <!-- Theme Style CSS -->
     <link rel="stylesheet" href="assets/css/dark-theme.css" />
     <link rel="stylesheet" href="assets/css/semi-dark.css" />
     <link rel="stylesheet" href="assets/css/header-colors.css" />
-	<link rel="shortcut icon" href="{{ asset('assets/icon/icon.ico') }}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{ asset('assets/icon/icon.ico') }}" type="image/x-icon">
 
     <title>Iglesia sansare</title>
+    <style>
+        footer {
+            color: white;
+            text-align: start;
+            padding: 10px;
+        }
+		footer a{
+			background-color: #ffe6a7 !important;
+        }
+    </style>
 </head>
 
 <body>
-	<!--wrapper-->
-	<div class="wrapper">
-		<!--start header -->
-		@include("layouts.header")
-		<!--end header -->
-		<!--navigation-->
-		@include("layouts.nav")
-		<!--end navigation-->
-		<!--start page wrapper -->
-		@yield("wrapper")
-		<!--end page wrapper -->
+    <!--wrapper-->
+    <div class="wrapper">
+        <!--start header -->
+        @include('layouts.header')
+        <!--end header -->
+        <!--navigation-->
+        @include('layouts.nav')
+        <!--end navigation-->
+        <!--start page wrapper -->
+        @yield('wrapper')
+        <!--end page wrapper -->
         <!-- Search Modal -->
         <!-- End Search Model -->
-		<!--start overlay-->
-
-	</div>
-	<!--end wrapper-->
+        <!--start overlay-->
+    </div>
+    <footer class="sticky-bottom ">
+        <a href="dashboard" class="btn border">
+            <img src="{{ asset('/assets/icon/Vector.svg') }}"  alt="" >
+        </a>
+		
+    </footer>
+    <!--end wrapper-->
     <!--start switcher-->
     <!--end switcher-->
-	<!-- Bootstrap JS -->
-	<script src="assets/js/bootstrap.bundle.min.js"></script>
-	<!--plugins-->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
-	<script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
-	<script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
-	<!--app JS-->
-	<script src="assets/js/app.js"></script>
-	@yield("script")
+    <!-- Bootstrap JS -->
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <!--plugins-->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/plugins/simplebar/js/simplebar.min.js"></script>
+    <script src="assets/plugins/metismenu/js/metisMenu.min.js"></script>
+    <script src="assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js"></script>
+    <!--app JS-->
+    <script src="assets/js/app.js"></script>
+    @yield('script')
 </body>
 
 </html>
