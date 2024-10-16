216f17372af5caf2c6e952ceb8c3dc4f24f77807
axelherreram <aherreram22@miumg.edu.gt>
2024-10-15 19:39:40 -0600

    Creación de factories para la tabla de bautizo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         $this->call(deptos::class);
         $this->call(municipioSeed::class);
         $this->call(InfoIglesia::class);
+        $this->call(BautizoSeeder::class);
     }
 }
